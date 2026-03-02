893e13add0e98e49708eae12f321d970d566c497
Maab-7 <182756355+Maab-7@users.noreply.github.com>
2026-03-01 20:08:52 -0400

    Mejora al maximizar la ventana del browser

--- src/test/java/base/DriverManager.java
+++ src/test/java/base/DriverManager.java
@@ -1,7 +1,11 @@
 package base;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,12 +19,22 @@ public class DriverManager {
     }
 
     public static void initDriver(String browser) {
+    	
+    	String headless = System.getProperty("headless");
 
         switch (browser.toLowerCase()) {
 
             case "chrome":
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                
+                if ("true".equalsIgnoreCase(headless)) {
+					options.addArguments("--headless=new");
+					options.addArguments("--window-size=1920,1080");
+					options.addArguments("--no-sandbox");
+					options.addArguments("--disable-dev-shm-usage");
+				}
+                driver = new ChromeDriver(options);
                 break;
 
             case "edge":
@@ -36,8 +50,10 @@ public class DriverManager {
             default:
                 throw new IllegalArgumentException("Browser no soportado");
         }
-
-        driver.manage().window().maximize();
+        //Tamaño de ventana consistente en cualquier entorno.
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.manage().window().maximize();
     }
 
     public static void quitDriver() {
